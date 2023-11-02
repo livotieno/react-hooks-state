9af73bf7e1688762c7ee151a7ffae03dc1596f61
livotieno <livio5261@gmail.com>
2023-11-02 09:56:19 +0300

    add

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -4,7 +4,16 @@ function Counter() {
   const [count, setCount] = useState(0);
 
   function increment() {
-    setCount(count + 1);
+
+    // setCount(count + 1);
+
+    // update state based on previous state
+    // setCount is async/takes some time
+    // it executes function first before updating state
+
+    setCount((prevState) => {
+      return prevState + 1;
+    });
   }
 
   return <button onClick={increment}>I have been clicked {count} times</button>;
